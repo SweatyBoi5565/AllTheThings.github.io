57ba4d08183364c9a9f78ba3e9ed9af66c2b72f9
SweatyBoi5565 <112290624+SweatyBoi5565@users.noreply.github.com>
2025-02-15 00:19:51 -0600

    Add files via upload

---
+++ v0.2/images/v0.3/index-script.js
@@ -0,0 +1,18 @@
+document.getElementById("nav1").addEventListener("click", function () {
+    window.location.href = "image.html";
+  });
+  document.getElementById("nav2").addEventListener("click", function () {
+    window.location.href = "music.html";
+  });
+  document.getElementById("nav3").addEventListener("click", function () {
+    window.location.href = "paint.html";
+  });
+  document.getElementById("nav4").addEventListener("click", function () {
+    window.location.href = "shop.html";
+  });
+  document.getElementById("nav5").addEventListener("click", function () {
+    window.location.href = "weather.html";
+  });
+  document.getElementById("nav6").addEventListener("click", function () {
+    window.location.href = "search.html";
+  });
